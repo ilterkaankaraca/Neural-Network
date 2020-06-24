c3257125372d3043b3eda89bdba0ed1ba3f1691a
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2020-06-24 23:22:57 +0300

    create an enum and a separated class for activation functions

---
+++ c#/NeuralNetwork/ActivationFunctions.cs
@@ -0,0 +1,24 @@
+namespace NeuralNetwork
+{
+        public class ActivationFunctions
+        {
+
+            public static void Sigmoid(Matrix temp)
+            {
+                for (int i = 0; i < temp.row; i++)
+                {
+                    for (int j = 0; j < temp.column; j++)               
+                        temp.matrix[i, j] = 1f / (1f + (float)Math.Exp(-temp.matrix[i, j]));
+                }
+            }
+            public static void Tanh(Matrix temp)
+            {
+                for (int i = 0; i < temp.row; i++)
+                {
+                    for (int j = 0; j < temp.column; j++)               
+                        temp.matrix[i, j] = 2f / (1f + (float)Math.Exp(-2f*temp.matrix[i, j]))-1;
+                }
+            }
+
+        }
+}
---
+++ c#/NeuralNetwork/ActivationType.cs
@@ -0,0 +1,11 @@
+namespace NeuralNetwork{
+/**
+ *
+ * @author tnc
+ */
+    public enum ActivationType
+    {
+        TANH,
+        SOFTMAX
+    }
+}
