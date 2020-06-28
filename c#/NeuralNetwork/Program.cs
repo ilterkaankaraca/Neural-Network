29e0c8ef4f70c6bf2c4ea7e0c4a474b50e00b600
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2020-06-28 22:18:46 +0300

    Update Program.cs

--- c#/NeuralNetwork/Program.cs
+++ c#/NeuralNetwork/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace NeuralNetwork
 {
     class Program
@@ -7,6 +6,9 @@ namespace NeuralNetwork
         static void Main(string[] args)
         {
             NeuralNetwork NN = new NeuralNetwork();
+            NN.Add(new Layer(2, ActivationType.SIGMOID,.5f));//input
+            NN.Add(new Layer(4, ActivationType.TANH,.25f));
+            NN.Add(new Layer(2, ActivationType.SIGMOID,1));
         }
     }
 }
