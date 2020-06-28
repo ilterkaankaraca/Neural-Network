531cec2f4a3468ccb19835593e9729770c8746ad
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2020-06-28 23:16:36 +0300

    create Layer.ApplyActivation and Layer.ApplyDerActivation functions

--- c#/NeuralNetwork/Layer.cs
+++ c#/NeuralNetwork/Layer.cs
@@ -1,16 +1,45 @@
+using System;
 namespace NeuralNetwork
 {
         public class Layer
         {
+                int loc;
                 int neuronNumber;
                 Matrix input;
                 Matrix output;
                 Matrix weight;
                 Matrix bias;
-                string activationFunc;
-                public Layer(int neuronNumber,string activationFunc)
+                ActivationType activationFunc;
+                public Layer(int neuronNumber, ActivationType activationFunc, float learning_rate)
                 {
                         this.neuronNumber=neuronNumber;
+                        this.activationFunc=activationFunc;
+                }
+                public Matrix ApplyActivation(Matrix input)
+                {
+
+                        if(activationFunc==ActivationType.SIGMOID)
+                        {
+                                return ActivationFunctions.Sigmoid(input);
+                        }
+                        else if(activationFunc==ActivationType.TANH)
+                        {
+                                return ActivationFunctions.Tanh(input);
+                        }
+                        return null;
+                }
+                public Matrix ApplyDerActivation(Matrix input)
+                {
+
+                        if(activationFunc==ActivationType.SIGMOID)
+                        {
+                                return ActivationFunctions.DerSigmoid(input);
+                        }
+                        else if(activationFunc==ActivationType.TANH)
+                        {
+                                return ActivationFunctions.DerTanh(input);
+                        }
+                        return null;
                 }
         }
 }
