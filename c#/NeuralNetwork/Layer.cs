655ebe9a5ffcf6aba2ceb47d56d3bbd17f531e78
Ilterkaan Karaca <ilterkaankaraca@gmail.com>
2020-06-29 11:44:16 +0300

    create Layer.Initialize

--- c#/NeuralNetwork/Layer.cs
+++ c#/NeuralNetwork/Layer.cs
@@ -3,8 +3,8 @@ namespace NeuralNetwork
 {
         public class Layer
         {
-                int loc;
-                int neuronNumber;
+                int neuronN;
+                Matrix layer;
                 Matrix input;
                 Matrix output;
                 Matrix weight;
@@ -12,12 +12,12 @@ namespace NeuralNetwork
                 ActivationType activationFunc;
                 public Layer(int neuronNumber, ActivationType activationFunc, float learning_rate)
                 {
-                        this.neuronNumber=neuronNumber;
+                        this.neuronN=neuronNumber;
                         this.activationFunc=activationFunc;
                 }
                 public Matrix ApplyActivation(Matrix input)
                 {
-
+                
                         if(activationFunc==ActivationType.SIGMOID)
                         {
                                 return ActivationFunctions.Sigmoid(input);
@@ -30,7 +30,7 @@ namespace NeuralNetwork
                 }
                 public Matrix ApplyDerActivation(Matrix input)
                 {
-
+                
                         if(activationFunc==ActivationType.SIGMOID)
                         {
                                 return ActivationFunctions.DerSigmoid(input);
@@ -41,5 +41,23 @@ namespace NeuralNetwork
                         }
                         return null;
                 }
-        }
+                public void Initialize(int thisLayerN, int nextLayerN)
+                {
+                        this.layer = new Matrix(thisLayerN, 1);
+
+                        this.weight = new Matrix(nextLayerN, thisLayerN); 
+                        this.bias = new Matrix(thisLayerN, 1); 
+
+                        this.weight.Randomize();
+                        this.bias.Randomize();
+                }
+                public void Initialize(int nextLayerN)
+                {
+                        this.layer = new Matrix(neuronN, 1);
+                        this.weight = new Matrix(nextLayerN, neuronN); 
+                        this.bias = new Matrix(neuronN, 1); 
+                        this.weight.Randomize();
+                        this.bias.Randomize();
+                }
+    }
 }
